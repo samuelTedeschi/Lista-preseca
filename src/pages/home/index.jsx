0491d3bc7f43a148dfbde4c30c4c4513e981e8df
samuel tedeschi <samueltedeschi100@yahoo.com>
2022-07-25 17:56:02 -0300

    1.0.1

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,4 +1,4 @@
-import React,{ useState } from 'react';
+import React,{ useState, useEffect } from 'react';
 import './style.css';
 import { Card } from '../../components/card';
 
@@ -6,6 +6,7 @@ export function App() {
 
   const [studentName, setStudentName] = useState('');
   const [students, setStudents] = useState([]);
+  const [user , setUser] = useState({name: '', avatar: ''})
 
   function handleAddStudent(){
       const newStudent = {
@@ -19,23 +20,49 @@ export function App() {
 
       setStudents(prevState => [...prevState, newStudent])
   }
+
+  useEffect( () => {
+
+    fetch('https://api.github.com/users/samuelTedeschi')
+    .then(response => response.json())
+    .then(data => {
+        setUser({
+          name: data.name,
+          avatar: data.avatar_url,
+        })
+    })
+  }, [] )
   
   return (
       <div className='container'>
-        <h1>Lista de Presença</h1>
+        <header>
+          <h1>Lista de Presença</h1>
+
+            <div>
+              <strong> {user.name} </strong>
+              <img src={user.avatar} alt="" />
+            </div>
+        </header>
 
         <input 
-        type="text" 
-        placeholder="Digite o nome" 
-        onChange={e => setStudentName(e.target.value)}
+          type="text" 
+          placeholder="Digite o nome" 
+          onChange={e => setStudentName(e.target.value)}
         />
 
         <button type="button" onClick={handleAddStudent}>
           Adicionar
         </button>
 
-        {
-          students.map(student => <Card name={student.name} time={student.time} />)
+        {students.map(student => 
+            (<Card
+
+              key={student.time}
+              name={student.name} 
+              time={student.time} 
+              />
+            )
+          )
         }
       </div>  
   )
